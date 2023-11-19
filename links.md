e02cbba68e5131a8da310659c46390757c921094
ruforester <ruforester@gmail.com>
2023-11-19 21:03:27 +0200

    bulma -> spectre

---
+++ links.md
@@ -0,0 +1,57 @@
+## Полезные ссылки
+========================
+
+### Загрузка Linux
+------------------------
+
+* [Протокол загрузки Linux/x86](https://www.kernel.org/doc/Documentation/x86/boot.txt)
+* [Параметры ядра Linux](https://github.com/torvalds/linux/blob/master/Documentation/admin-guide/kernel-parameters.rst)
+
+Защищённый режим
+------------------------
+
+* [64-ia-32-architectures-software-developer-vol-3a-part-1-manual.pdf](http://www.intel.com/content/www/us/en/processors/architectures-software-developer-manuals.html)
+
+Управление памятью в ядре Linux
+--------------------------------------
+
+* [Заметки @lorenzo-stoakes о подсистеме VM в ядре Linux](https://github.com/lorenzo-stoakes/linux-vm-notes)
+
+Программирование последовательного порта
+------------------------
+
+* [Программирование 8250 UART](http://en.wikibooks.org/wiki/Serial_Programming/8250_UART_Programming#UART_Registers)
+* [Последовательные порты на OSDEV](http://wiki.osdev.org/Serial_Ports)
+
+VGA
+------------------------
+
+* [Video Graphics Array (VGA)](http://en.wikipedia.org/wiki/Video_Graphics_Array)
+
+Ввод/вывод
+------------------------
+
+* [Программирвоание портов ввода/вывода](http://www.tldp.org/HOWTO/text/IO-Port-Programming)
+
+GCC и GAS
+------------------------
+
+* [Типы атрибутов GCC](https://gcc.gnu.org/onlinedocs/gcc/Type-Attributes.html)
+* [Директивы ассемблера](http://www.chemie.fu-berlin.de/chemnet/use/info/gas/gas_toc.html#TOC65)
+
+
+Важные структуры данных
+--------------------------
+
+* [Определение task_struct](http://lxr.free-electrons.com/source/include/linux/sched.h#L1274)
+
+Управление памятью
+------------------------
+
+* [Серия статей "NUMA Deep Dive"](http://frankdenneman.nl/2016/07/07/numa-deep-dive-part-1-uma-numa/)
+
+Полезные ссылки
+------------------------
+
+* [Запуск программ в Linux x86](http://dbp-consulting.com/tutorials/debugging/linuxProgramStartup.html)
+* [Разметка памяти при выполнении программы (32 бита)](http://fgiasson.com/articles/memorylayout.txt)
---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	// router := gin.Default()
+	e := echo.New()
+	// router.Static("/", "./static")
+	e.Static("/", "static")
+	// router.LoadHTMLGlob("templates/*.html")
+	e.File("/", "templates/spectre.html")
+
+	e.GET("/links", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, csvToHtml("links.csv", "a", ','))
+	})
+
+	e.GET("/reqs", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, "<h2></h2>")
+	})
+
+	e.GET("/dir", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, csvToHtml("example2.csv", "table", ','))
+	})
+
+	print(csvToHtml("example2.csv", "table", ','))
+
+	e.Logger.Fatal(e.Start(":1323"))
+
+	// router.GET("/dir", func(ctx *gin.Context) {
+	// 	ctx.Data(http.StatusOK, "text/html", csvToHtml("example.csv"))
+	// })
+
+	// router.GET("/links", func(ctx *gin.Context) {
+	// 	ctx.Data(http.StatusOK, "text/html", mdToHtml("links.md"))
+	// })
+
+	// router.Run(":8080")
+
+}
+
+func csvToHtml(csvPath, htmlElement string, sep rune) string {
+	f, err := os.Open(csvPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+	r := csv.NewReader(f)
+	r.Comma = sep
+	r.LazyQuotes = true
+	s, err := r.ReadAll()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if htmlElement == "table" {
+		htmlTable := "<table class='table table-striped table-scroll table-hover'>"
+		for i, row := range s {
+			if i > 10 {
+				break
+			}
+			htmlTable = htmlTable + "<tr>"
+			for _, col := range row {
+				// if j > 5 {
+				// 	break
+				// }
+				htmlTable = htmlTable + fmt.Sprintf("<td>%s</td>", col)
+			}
+			htmlTable = htmlTable + "</tr>"
+		}
+		htmlTable = htmlTable + "</table>"
+		return htmlTable
+	}
+
+	href := "<ul>"
+	for i, row := range s {
+		if i == 0 {
+			continue
+		}
+		href = href + fmt.Sprintf("<li><a href='%s'>%s</a></li>", row[1], row[0])
+	}
+	href = href + "</ul>"
+	return href
+}
+
+// func mdToHtml(mdPath string) []byte {
+// 	f, _ := os.Open(mdPath)
+// 	b, _ := io.ReadAll(f)
+// 	mdBytes := blackfriday.Run(b)
+// 	return mdBytes
+// }
